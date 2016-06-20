657c774edfec50ad6d552b3432fee1912c7a079b
Alexey Kremsa <10kdmg@gmail.com>
2016-06-20 23:47:47 +0300

    Prepared initial solution

---
+++ Chat.All/Chat.Database/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(200) NOT NULL, 
+    [LastName] NVARCHAR(200) NOT NULL, 
+    [Email] NVARCHAR(200) NOT NULL, 
+    [PasswordHash] BINARY(32) NOT NULL, 
+    [PasswordSalt] NVARCHAR(200) NOT NULL
+)
